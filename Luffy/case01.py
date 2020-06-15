fc628d8e22b328762a5be3e473b7c423d144c9aa
miniLLS <13611887280@163.com>
2020-06-15 15:36:31 +0800

    第一次尝试提交

---
+++ Luffy/case01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import requests
+
+target_url = 'http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=母系社会'
+
+def info_collection(target_url):
+    '''
+    简易网页采集器，爬取指定词条对应的搜索结果页面。
+    '''
+
+    pass
